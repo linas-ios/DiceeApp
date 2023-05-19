2c0afc3636fd31e580ec5c97b4039160a0bdf6b7
linas-ios <linasn404@gmail.com>
2023-05-19 11:29:15 +0200

    add basic logic with random image in array

--- DiceApp/ViewController.swift
+++ DiceApp/ViewController.swift
@@ -9,11 +9,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  @IBOutlet weak var diceOneImage: UIImageView!
+  @IBOutlet weak var diceTwoImage: UIImageView!
+
+  let imagesArray = [
+    UIImage(named: "DiceOne"),
+    UIImage(named: "DiceTwo"),
+    UIImage(named: "DiceThree"),
+    UIImage(named: "DiceFour"),
+    UIImage(named: "DiceFive"),
+    UIImage(named: "DiceSix")
+  ]
+
+
   override func viewDidLoad() {
     super.viewDidLoad()
-    view.backgroundColor = .cyan
+
   }
 
+  @IBAction func rollButtonTapped(_ sender: UIButton) {
+    diceOneImage.image = imagesArray[Int.random(in: 0..<6)]
+    diceTwoImage.image = imagesArray[Int.random(in: 0..<6)]
+
+  }
 
 }
 
